5a94505fc7dd096eb657d33a45e0b8866cfb5b91
Liam Hampton <liam.hampton@ibm.com>
2019-10-31 22:33:52 +0000

    create basic hello world server page
    Signed-off-by: Liam Hampton <liam.hampton@ibm.com>

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	// main func
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8181", nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello world, this is a basic index page")
 }
