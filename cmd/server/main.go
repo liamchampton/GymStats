8f9b4325e2e727ea1d1a4b2d39523112cc931d92
Liam Hampton <liam.hampton@ibm.com>
2019-11-08 14:22:39 +0000

    make first html page from example
    Signed-off-by: Liam Hampton <liam.hampton@ibm.com>

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-
-	"github.com/gorilla/mux"
+	"text/template"
 )
 
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+
 func main() {
-	r := mux.NewRouter()
+	//r := mux.NewRouter()
 
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-		fmt.Fprintf(w, "hello world")
+	tmpl := template.Must(template.ParseFiles("../client/index.html"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := TodoPageData{
+			PageTitle: "My TODO list",
+			Todos: []Todo{
+				{Title: "Task 1", Done: false},
+				{Title: "Task 2", Done: false},
+				{Title: "Task 3", Done: true},
+			},
+		}
+		tmpl.Execute(w, data)
 	})
 
-	http.ListenAndServe(":8181", r)
+	http.ListenAndServe(":8181", nil)
 }
